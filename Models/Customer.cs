5ac5277f479fc2d5ee1a3b786690dd55ea7d7c42
pixelboogie <justamovie@yahoo.com>
2024-06-17 16:18:17 -0500

    Added connection to local server & db

---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreEntityFrameworkDatabaseFirstApproach.Models;
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; } = null!;
+    public double? CustomerAmount { get; set; }
+}
---
+++ Models/DatabaseFirstDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace CoreEntityFrameworkDatabaseFirstApproach.Models;
+
+public class DatabaseFirstDbContext : DbContext
+{
+    public DatabaseFirstDbContext()
+    {
+
+    }
+  //  public DatabaseFirstDbContext(DbContextOptions<DatabaseFirstDbContext>()) : base(options)   {  } NOT FINISHED
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\ButtersDB;Database=DatabaseFirstDB;Integrated Security=true; TrustServerCertificate = true;");
+    }
+
+
+}
